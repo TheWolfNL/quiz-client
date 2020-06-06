e2613f8e1bd018a7a0a69b36d8ce6adaa4332fee
Tjeerd Ritsma <tjeerd.ritsma@quinyx.com>
2020-06-06 15:00:24 +0200

    Updates

---
+++ precache-manifest.3ea9caf7816d9c0a0c10cc0381640f59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f790e28991c9d217f5e8399bfdd4a24",
+    "url": "/quiz-client/index.html"
+  },
+  {
+    "revision": "c93c93df03775f09b7e5",
+    "url": "/quiz-client/static/css/main.2aa4ddd3.chunk.css"
+  },
+  {
+    "revision": "ca9cb2fc3d491191c47b",
+    "url": "/quiz-client/static/js/2.8dbd77d8.chunk.js"
+  },
+  {
+    "revision": "c93c93df03775f09b7e5",
+    "url": "/quiz-client/static/js/main.9df71240.chunk.js"
+  },
+  {
+    "revision": "abae039bd1e405515600",
+    "url": "/quiz-client/static/js/runtime~main.87fdd00e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quiz-client/static/media/logo.5d5d9eef.svg"
+  }
+]);
